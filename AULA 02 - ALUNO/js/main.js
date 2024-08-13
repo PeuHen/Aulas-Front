d159275feae2707a684d9e854d685dd031b91a0c
PeuHen <165822649+PeuHen@users.noreply.github.com>
2024-08-13 20:52:57 -0300

    upando antes do intervalo

--- AULA 02 - ALUNO/js/main.js
+++ AULA 02 - ALUNO/js/main.js
@@ -1,17 +1,17 @@
 function getDados() {
-    //* funcao
+  //* funcao
 
-    //  alert("Ola mundo"); // *msg na tela
-    // console.log("ola novamente"); //* msg no console (f12)
-    // confirm("Deseja salvar os dados"); // *msg com interação
-    // var teste = prompt("Digite um numero"); // *prompt = input do JS / var = criar variavel
-    // console.log(teste); // *mostrando oq digitei no prompt
-    // var nome = document.getElementById("nome").value; // *.value = pegar o VALOR
-    // var email = document.getElementById("email").value;
-    // console.log(nome)
-    // console.log(email)
+  //  alert("Ola mundo"); // *msg na tela
+  // console.log("ola novamente"); //* msg no console (f12)
+  // confirm("Deseja salvar os dados"); // *msg com interação
+  // var teste = prompt("Digite um numero"); // *prompt = input do JS / var = criar variavel
+  // console.log(teste); // *mostrando oq digitei no prompt
+  // var nome = document.getElementById("nome").value; // *.value = pegar o VALOR
+  // var email = document.getElementById("email").value;
+  // console.log(nome)
+  // console.log(email)
 
-/*
+  /*
 ? = atribuit valor
 ? == igualdade
 ? > maior
@@ -24,7 +24,19 @@ function getDados() {
 ? ==! conteudos igual e tipo de dado diferente
 ? !=! diferença de conteudos e se os tipos de dados sao diferentes
 */
-    if (nome == "" || email == ''){
-        alert("Preencha seus dados")
-    }
+
+  /*
+  ! DICA: usar o console.log para descobrir oq a nuva funcao faz
+
+? isNaN() = NotANumber
+
+
+*/
+  if (nome == "" || email == "") {
+    alert("Preencha seus dados");
+  }
+}
+
+function setBlockNumber() {
+  console.log("teste");
 }
