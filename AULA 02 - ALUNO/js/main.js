fd0e9a30fb4a08645c3653e5601ccc9775da02d9
PeuHen <165822649+PeuHen@users.noreply.github.com>
2024-08-13 20:37:06 -0300

    Salvando antes do intervalo

--- AULA 02 - ALUNO/js/main.js
+++ AULA 02 - ALUNO/js/main.js
@@ -1,17 +1,30 @@
 function getDados() {
+    //* funcao
 
-    //funcao
-    alert("Ola mundo"); // *msg na tela
-    console.log("ola novamente"); //* msg no console (f12)
-    confirm("Deseja salvar os dados"); // *msg com interação
-    var teste = prompt("Digite um numero"); // *prompt = input do JS / var = criar variavel
-    console.log(teste); // *mostrando oq digitei no prompt
-    var nome = document.getElementById("nome").value; // *.value = pegar o VALOR
-    var email = document.getElementById("email").value;
-    console.log(nome)
-    console.log(email)
+    //  alert("Ola mundo"); // *msg na tela
+    // console.log("ola novamente"); //* msg no console (f12)
+    // confirm("Deseja salvar os dados"); // *msg com interação
+    // var teste = prompt("Digite um numero"); // *prompt = input do JS / var = criar variavel
+    // console.log(teste); // *mostrando oq digitei no prompt
+    // var nome = document.getElementById("nome").value; // *.value = pegar o VALOR
+    // var email = document.getElementById("email").value;
+    // console.log(nome)
+    // console.log(email)
 
-    if (nome == ""){
-        alert("Preencha o seu  nome")
+/*
+? = atribuit valor
+? == igualdade
+? > maior
+? < menor
+? != diferente
+? <= maior oi igual
+? >= menor ou igual
+? === comparação o tipo de dado  e conteudo 
+? !==  conteudo diferente e tipo de dado igual
+? ==! conteudos igual e tipo de dado diferente
+? !=! diferença de conteudos e se os tipos de dados sao diferentes
+*/
+    if (nome == "" || email == ''){
+        alert("Preencha seus dados")
     }
 }
