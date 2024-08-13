46259c2a10bcb3e40bc32ed2c6e53e233b6fd487
PeuHen <165822649+PeuHen@users.noreply.github.com>
2024-08-13 20:11:49 -0300

    Add files via upload

--- AULA 02 - ALUNO/js/main.js
+++ AULA 02 - ALUNO/js/main.js
@@ -1,13 +1,17 @@
 function getDados() {
 
     //funcao
-    alert("Ola mundo"); //msg na tela
-    console.log("ola novamente"); //msg no console (f12)
-    confirm("Deseja salvar os dados"); // msg com interação
-    var teste = prompt("Digite um numero"); // input do JS / var criar variavel
-    console.log(teste); // mostrando oq digitei no prompt
-    var nome = document.getElementById("nome").value;
+    alert("Ola mundo"); // *msg na tela
+    console.log("ola novamente"); //* msg no console (f12)
+    confirm("Deseja salvar os dados"); // *msg com interação
+    var teste = prompt("Digite um numero"); // *prompt = input do JS / var = criar variavel
+    console.log(teste); // *mostrando oq digitei no prompt
+    var nome = document.getElementById("nome").value; // *.value = pegar o VALOR
     var email = document.getElementById("email").value;
     console.log(nome)
     console.log(email)
+
+    if (nome == ""){
+        alert("Preencha o seu  nome")
+    }
 }
