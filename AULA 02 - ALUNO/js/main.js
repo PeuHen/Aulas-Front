f187ab314ce2c51e893a9b7f9c5cbc3b0538b480
PeuHen <165822649+PeuHen@users.noreply.github.com>
2024-08-13 19:43:05 -0300

    PASTA

---
+++ AULA 02 - ALUNO/js/main.js
@@ -0,0 +1,13 @@
+function getDados() {
+
+    //funcao
+    alert("Ola mundo"); //msg na tela
+    console.log("ola novamente"); //msg no console (f12)
+    confirm("Deseja salvar os dados"); // msg com interação
+    var teste = prompt("Digite um numero"); // input do JS / var criar variavel
+    console.log(teste); // mostrando oq digitei no prompt
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    console.log(nome)
+    console.log(email)
+}
